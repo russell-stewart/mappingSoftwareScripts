b21a2ae34cb985c8ae00189ee22dd3e12f9e8b72
Russell Stewart <russells98@gmail.com>
2017-07-24 11:28:57 -0600

    this seems to be a working version????
    knock on wood lol

--- hisat_map.py
+++ hisat_map.py
@@ -3,7 +3,7 @@
 #7/18/2017
 #Executes HISAT mapping software for trimmed fastq files (outputted by skewer)
 #Before running this, make sure that you have built a HISAT index:
-#/path/to/hisat/directory/hisat-build -f /data/reference/iGenomes/Homo_sapiens/UCSC/hg38/Sequence/WholeGenomeFasta/genome.fa Homo_sapiens
+#/path/to/hisat/directory/hisat-build -f /data/reference/iGenomes/Homo_sapiens/UCSC/hg38/Sequence/WholeGenomeFasta/genome.fa Homo_sapiens_index
 #HISAT reference manual:
 #http://ccb.jhu.edu/software/hisat/manual.shtml
 #Download HISAT on Github:
@@ -16,9 +16,9 @@ import subprocess
 srcDir = '/Seibold/proj/Russell_playground/skewer'
 destDir = '/Seibold/proj/Russell_playground/hiSatAligner'
 runThreadN = 12
-hisatIndexNamePrefix = 'Homo_sapiens'
+hisatIndexNamePrefix = 'Homo_sapiens_index'
 email = 'russells98@gmail.com'
-gtfFile='/data/reference/iGenomes/Homo_sapients/UCSC/hg38/Annotation/Genes/genes.gtf'
+gtfFile='/data/reference/iGenomes/Homo_sapiens/UCSC/hg38/Annotation/Genes/genes.gtf'
 
 #makes and returns a new directory with given directory path and name of dir
 def mkDir(path , name):
@@ -51,7 +51,7 @@ for folder in folders:
 #run HISAT for each sample
 j = 0
 for sample in samples:
-    command = "bsub -u %s -R \"rusage[mem=30000]\" '/Seibold/proj/Russell_playground/hiSatAligner/hisat/hisat -q -t -p %d --known-splicesite-infile %s -x %s -1 %s -2 %s -S %s'" % (email , runThreadN , gtfFile , hisatIndexNamePrefix , srcDir + '/' + sample[0] , srcDir + '/' + sample[1] , sampleDestDir[j] + '/'  + '_SAM_out.sam')
+    command = "bsub -u %s -R \"rusage[mem=30000]\" '/Seibold/proj/Russell_playground/hiSatAligner/hisat/hisat -q -t -p %d --known-splicesite-infile %s -x %s -1 %s -2 %s -S %s'" % (email , runThreadN , gtfFile , hisatIndexNamePrefix , srcDir + '/' + sample[0] , srcDir + '/' + sample[1] , sampleDestDir[j] + '/' + sample[1][:sample[1].find('/')]  + '_SAM_out.sam')
     print command + '\n'
     coreID = subprocess.check_output(command , shell=True)
     print coreID
