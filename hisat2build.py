57095b83116b55a2fea535839637eb01d24a2135
Russell Stewart <russells98@gmail.com>
2017-07-27 08:36:02 -0600

    add full snp/haplotype support for hisat2build and hisat2map

--- hisat2build.py
+++ hisat2build.py
@@ -1,21 +1,27 @@
+#hisat2build.py
+#Russell O. Stewart
+#7/27/2017
+#Builds hisat2 indexes needed to run hisat2 mapping. These index files also
+#incorporate haplotypes and SNPs.
+#Make sure you have converted your dbSNP file into hisat's snp format using
+#hisat2_extract_snps_haplotypes_UCSC.py (part of HISAT2) BEFORE running this!
+#note: index files will appear in the directory in which you run this script!
+#preferably, run this inside of the hisat2 directory itself
 import os
 import subprocess
-
-#note: index files will appear in the directory in which you run this script!
 
 email = 'russells98@gmail.com'#email for openlava output
 pathToHisat2 = '/Seibold/proj/Russell_playground/hisat2/hisat2-2.1.0'#where your hisat2 dierectory is stored
 
 threads = 12#how many threads are available to run this on?
 genomeFastaFile = '/data/reference/iGenomes/Homo_sapiens/UCSC/hg38/Sequence/WholeGenomeFasta/genome.fa'#path to the fasta file
-baseName = 'Homo_sapiens_index_with_dbSNP'#what do you want to name the index files?
+baseName = 'Homo_sapiens_index_with_SNP'#what do you want to name the index files?
 
-dbSNPFile = '/data/reference/iGenomes/Homo_sapiens/UCSC/hg38/Annotation/Variation/dbSNP146_common.txt'#path to SNPs, in dbSNP format.
-snpBaseFilename = 'dbSNP146_common'
+snpFile = '/Seibold/proj/Russell_playground/hisat2/hisat2IndexWithDBSNP/SNP144_common.snp'#path to SNPs, in HISAT2's format, .snp
+haplotypeFile = '/Seibold/proj/Russell_playground/hisat2/hisat2IndexWithDBSNP/SNP144_common.haplotype'#same but .haplotype for haplotype
 
-dbSNPToHisatFormatCommand = "bsub -u %s -R \"rusage[mem=30000]\" 'python %s/hisat2_extract_snps_haplotypes_UCSC.py %s %s %s'" %(g)
-
-indexBuildCommand = "bsub -u %s -R \"rusage[mem=30000]\" '%s/hisat2-build' -f -p %d --snp %s %s %s" % (email , pathToHisat2 , threads , dbSNPFile , genomeFastaFile , baseName)
+indexBuildCommand = "bsub -u %s -R \"rusage[mem=40000]\" '%s/hisat2-build' -f -p %d --snp %s  --haplotype %s %s %s" % (email , pathToHisat2 , threads , snpFile , haplotypeFile , genomeFastaFile  , baseName)
 
 coreID = subprocess.check_output(indexBuildCommand , shell = True)
+print indexBuildCommand
 print coreID
--- hisat2map.py
+++ hisat2map.py
@@ -1,15 +1,22 @@
+#hisat2map.py
+#Russell O. Stewart
+#7/27/2017
+#Batch runs hisat2 mapping software for a set of trimmed fastq files outputted
+#from skewer.
+#hisat2 is available from:
+#https://github.com/infphilo/hisat2
+
 import os
 import time
 import subprocess
 
 #make sure to update these!!!
-pathToHisat2 = '/Seibold/proj/Russell_playground/hisat2/hisat2-2.1.0'
-srcDir = '/Seibold/proj/Russell_playground/skewer'
-destDir = '/Seibold/proj/Russell_playground/hisat2'
-runThreadN = 12
-hisatIndexNamePrefix = 'Homo_sapiens_index'
-email = 'russells98@gmail.com'
-gtfFile='/data/reference/iGenomes/Homo_sapiens/UCSC/hg38/Annotation/Genes/genes.gtf'
+pathToHisat2 = '/Seibold/proj/Russell_playground/hisat2/hisat2-2.1.0'#where is hisat2 stored?
+srcDir = '/Seibold/proj/Russell_playground/skewer'#where are the trimmed fastq files stored?
+destDir = '/Seibold/proj/Russell_playground/hisat2/withSNP'#where do you want the results to go?
+runThreadN = 12#how many processor cores are available?
+hisatIndexNamePrefix = 'Homo_sapiens_index_with_SNP'#what is the base name of your hisat2 index files?
+email = 'russells98@gmail.com'#email for openlava output
 
 #makes and returns a new directory with given directory path and name of dir
 def mkDir(path , name):
@@ -42,7 +49,7 @@ for folder in folders:
 #run HISAT2 for each sample
 j = 0
 for sample in samples:
-    command = "bsub -u %s -R \"rusage[mem=6000]\" '%s/hisat2 -q -t -p %d -x %s -1 %s -2 %s -S %s'" % (email , pathToHisat2 , runThreadN , pathToHisat2 + '/' + hisatIndexNamePrefix , srcDir + '/' + sample[0] , srcDir + '/' + sample[1] , sampleDestDir[j] + '/' + sample[1][:sample[1].find('/')]  + '_SAM_out.sam')
+    command = "bsub -u %s -R \"rusage[mem=10000]\" '%s/hisat2 -q -t -p %d -x %s -1 %s -2 %s -S %s'" % (email , pathToHisat2 , runThreadN , pathToHisat2 + '/' + hisatIndexNamePrefix , srcDir + '/' + sample[0] , srcDir + '/' + sample[1] , sampleDestDir[j] + '/' + sample[1][:sample[1].find('/')]  + '_SAM_out.sam')
     print command + '\n'
     coreID = subprocess.check_output(command , shell=True)
     print coreID
